88410dfddfbc78087dc76e07cf811d61edf4a25a
Hasan Cem P <hasancempinar@gmail.com>
2026-04-10 17:28:18 +0300

    README updated

---
+++ Week02-Collections/Day06.1-ChallengeProject/Program.cs
@@ -0,0 +1,7 @@
+﻿//🏆 Hafta 2 — Meydan Okuma Projesi: "Mini Depo Yönetim"
+//Bu sefer tek bir "txt" değil, iki farklı "txt" olacak!
+
+//Enum: IslemTipi { Giris, Cikis, Duzeltme, Silme }
+//Dictionary: Stok listesi: Dictionary<string, int> stoklar(Örn: "Pamuk Kumaş"-> 150)
+//Log Listesi: Her işlem yapıldığında bunu "loglar.txt" dosyasına o anki tarih, saat ve IslemTipi ile birlikte kaydet. (Örn: 10.04.2026 15:00 - Pamuk Kumaş giriş yapıldı. Adet: +50)
+//Stokların son halini ise "stok.txt" dosyasından oku ve oraya kaydet. Görev: Kullanıcı menüden "Ürün Girişi", "Ürün Çıkışı", "Stok Listele", "Log Kayıtlarını Gör" seçeneklerini seçebilecek. Olmayan üründen çıkış yapılırsa veya miktar eksiye düşerse Exception fırlatıp kullanıcıyı try-catch ile uyar!
---
+++ Week03-OOP/Day01-ClassesAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿//1:  "Telefon" adında bir class oluştur. (Marka, Model, Fiyat, BataryaKapasitesi özellikleri olsun).
+//2:  "AramaYap(string numara)" adında bir metodu olsun. "X numarası aranıyor..." yazsın.
+//3:  Ana programında(yukarıda) 2 farklı Telefon nesnesi oluştur, farklı değerler verip metodlarını çağır.
+//4:  "Ogrenci" adında bir class oluştur. (Ad, Soyad, Yas, OgrenciNo özellikleri olsun). Bir de "KendiniTanit()" metodu olsun (Adım X, numaram Y gibi).
+//5:  Şimdi Ogrenci nesnelerinden 3 tane üretip bunları List<Ogrenci> içine ekle. Foreach döngüsü ile hepsinin "KendiniTanit()" metodunu tek tek çağır!
+
+
+
+
+
+
+class Telefon
+{
+    public string Marka;
+    public string Model;
+    public int Fiyat;
+    public string BataryaKapasitesi;
+}
